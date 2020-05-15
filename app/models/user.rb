d3c8f7e0e5b61145920f24b9ea2836acb9a66f98
Sergey <sukyans@gmail.com>
2020-05-14 22:08:15 -0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :shape_user
+  has_many :shapes, through: :shape_user
 end
