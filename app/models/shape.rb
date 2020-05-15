ad84d4238cdb533098d6d1e065da57455e189bfe
Sergey <sukyans@gmail.com>
2020-05-15 15:53:04 -0600

    refactor shapes to add method

--- app/models/shape.rb
+++ app/models/shape.rb
@@ -7,6 +7,11 @@ class Shape < ApplicationRecord
 
   scope :active_shapes, -> { where(default_status: true).order(created_at: :asc) }
 
+  def self.shapes_to_add(current_user)
+    added_shapes = ShapeUser.where(user_id: current_user).pluck(:shape_id)
+    self.where.not(id: added_shapes)
+  end
+
   def human_boolean(boolean)
     boolean ? 'Yes' : 'No'
   end
