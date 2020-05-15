891cd7ae80badb045177e2b4beffaeb28f29aefc
Sergey <sukyans@gmail.com>
2020-05-15 12:26:37 -0600

    Update shape.rb

--- app/models/shape.rb
+++ app/models/shape.rb
@@ -2,5 +2,5 @@ class Shape < ApplicationRecord
   has_many :shape_user
   has_many :users, through: :shape_user
 
-  scope :active_shapes, -> { where(default_status: true).order(created_at: :desc) }
+  scope :active_shapes, -> { where(default_status: true).order(created_at: :asc) }
 end
