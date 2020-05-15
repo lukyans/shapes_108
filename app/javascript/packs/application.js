e8843773704dd8caa87e275c7f892c23b595a68f
Sergey <sukyans@gmail.com>
2020-05-15 14:01:35 -0600

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
+require("jquery-ui/ui/widget")
+require("jquery-ui/ui/widgets/sortable")
 
+$(document).on("turbolinks:load", () => {
+  $("#shapes").sortable();
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
