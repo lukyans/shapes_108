49a59722bc15b580fdc1a4483547bbcbfcf4d12c
Sergey <sukyans@gmail.com>
2020-05-15 16:41:47 -0600

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to launchpad_path(@current_launchpad)
+    end
     @shapes = Shape.active_shapes
   end
 end
